016da6100dabf421935e269ca5780c233c6bb983
kushal752 <kushaldabbe752@gmail.com>
2023-08-03 20:34:55 +0530

    exercises by bodypart

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,6 +6,18 @@ const SearchExercises = () => {
 
   const [search, setSearch] = useState('');
   const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+    
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExercisesData();
+  }, [])
+  
 
   const handleSearch = async () => {
     if (search) {
@@ -65,6 +77,9 @@ const SearchExercises = () => {
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{position: 'relative', width: '100%', p: '20px' }}>
+        <HorizontalScrollBar data={bodyParts}/>
       </Box>
     </Stack>
   )
